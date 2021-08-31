44aec7ef4b07c56643c52f6d5de78322d0f66be2
Pedram jabbarzadeh <p.jabbar78@gmail.com>
2021-08-31 13:44:44 +0430

    Import All Font Source

--- Dev_cardMVC/Controllers/HomeController.cs
+++ Dev_cardMVC/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Dev_cardMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
