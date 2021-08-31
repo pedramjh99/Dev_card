f038a394463627e9b42c7b7e8ba2fe9f9929b4b6
Pedram jabbarzadeh <p.jabbar78@gmail.com>
2021-08-31 12:50:00 +0430

    Repository initialize

---
+++ Dev_cardMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dev_cardMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
